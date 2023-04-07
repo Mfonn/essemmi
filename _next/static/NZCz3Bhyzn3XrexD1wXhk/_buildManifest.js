27c40c6853cd19a5cbae55515101097a8e3ad509
Mfonn <67243684+Mfonn@users.noreply.github.com>
2023-04-07 08:06:27 +0000

    Deploying to public from @ Mfonn/essemmi@ba2c0a227b710e3539f7e791769e8ee5991c4ea2 🚀

---
+++ _next/static/NZCz3Bhyzn3XrexD1wXhk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/912-946c06a7edb705f6.js","static/css/ae4ed9c503fd1e33.css","static/chunks/pages/index-71f55b4dcaf789b6.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
